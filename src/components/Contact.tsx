05267601caebcef518a542aca29a79ddc961ad76
Jannik Grausen Fischer <jgf@webtop.dk>
2018-12-22 16:56:56 +0100

    updated contacts components to support netlify forms

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,24 +1,59 @@
 import * as React from "react";
 
+const encode = (data) => {
+    return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+}
+
 export interface ContactProps {
-}export default class Contact extends React.Component<ContactProps, undefined> {
+    name: string,
+    email: string,
+    message: any
+
+}export default class Contact extends React.Component<ContactProps> {
+    constructor(ContactProps) {
+        super(ContactProps);
+        this.state = {name: "", email: "", message: ""};
+    }
+
+    handleSubmit = e => {
+        fetch("/", {
+          method: "POST",
+          headers: { "Content-Type": "application/x-www-form-urlencoded" },
+          body: encode({ "form-name": "contact", ...this.state })
+        })
+          .then(() => alert("Success!"))
+          .catch(error => alert(error));
+  
+        e.preventDefault();
+      };
+  
+    handleChange = e => this.setState({ [e.target.name]: e.target.value });  
 
     render() {
+        const { name, email, message } = this.state as ContactProps;
         return (
-            <div className="contact">
-                <form name="contact" method="POST" data-netlify="true">
-                    <p>
-                        <label>Email: <input type="text" name="name" /></label>
-                    </p>
-                    <p>
-                        <label>Message: <textarea name="message"></textarea></label>
-                    </p>
-                    <div data-netlify-recaptcha="true"></div>
-                    <p>
-                        <button type="submit">Send</button>
-                    </p>
-                </form>
-            </div>
+          <form onSubmit={this.handleSubmit}>
+            <p>
+              <label>
+                Your Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
+              </label>
+            </p>
+            <p>
+              <label>
+                Your Email: <input type="email" name="email" value={email} onChange={this.handleChange} />
+              </label>
+            </p>
+            <p>
+              <label>
+                Message: <textarea name="message" value={message} onChange={this.handleChange} />
+              </label>
+            </p>
+            <p>
+              <button type="submit">Send</button>
+            </p>
+          </form>
         );
     }
 }
